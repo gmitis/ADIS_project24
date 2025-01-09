876daecfdb6b386656b4688f4fa885bf88dddb45
gmitis <gi.muths@gmail.com>
2025-01-09 09:41:08 +0000

    Utils.DBPopulation: Created script that generates population scripts for each of the tables of the db

--- utils/generate_table_scripts/postgresql_population_scripts.sh
+++ utils/generate_table_scripts/postgresql_population_scripts.sh
@@ -1,28 +1,28 @@
 #! /bin/bash
 
 input_file="$HOME/ADIS_project24/datasources/tpcds.sql"
-output_dir="$HOME/ADIS_project24/datasources/population_scripts/postgres"
-tables_catalog="$HOME/ADIS_project24/data/csv_data/tables_catalog.txt"
-data_dir="$HOME/ADIS_project24/data/csv_data"
+output_dir="$HOME/ADIS_project24/datasources/table_population_scripts/postgresql"
+tables_catalog="$HOME/ADIS_project24/data/tables_catalog.txt"
+data_dir="/data"
 
 # create directory where table scripts are going to reside if it doesn't exist
-if [ ! -d "$output_dir" ]; then
-    mkdir -p "$output_dir"
-fi
+rm -rf "$output_dir" && mkdir -p "$output_dir"
 
 # make the files
 create_files() {
     local table="$1"
 
-    if grep -iq "$table" "$tables_catalog"; then     
+    if grep -iq "^$table$" "$tables_catalog"; then     
         # create output directory file
         sql_file="$output_dir/${table}.sql"
 
         # copy fields of table to file
-        sed -n "/create table $table/,/);/p" "$input_file" > "$sql_file"
+        sed -n "/^create table $table\\b/,/);/p" "$input_file" > "$sql_file"
+        sed -i "s/create table $table/create unlogged table $table/i" "$sql_file"
 
         # copy population command to file 
-        printf "\ncopy $table from '${data_dir}/${table}.csv' with (format csv, delimiter ',');\n" >> "$sql_file"
+        echo "copy $table from '${data_dir}/${table}.csv' with (format csv, delimiter ',');" >> "$sql_file"
+        printf "\n" >> "$sql_file"
 
         echo "Created $table.sql"
     else
