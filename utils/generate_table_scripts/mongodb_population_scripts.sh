0065c01f257a8044c11ab25e7e29c67fc1c08388
gmitis <gi.muths@gmail.com>
2025-01-11 18:19:09 +0000

    Created mongodb table population scripts

--- utils/generate_table_scripts/mongodb_population_scripts.sh
+++ utils/generate_table_scripts/mongodb_population_scripts.sh
@@ -1,41 +1,41 @@
-#!/bin/bash
+#! /bin/bash
 
 input_file="$HOME/ADIS_project24/datasources/tpcds.sql"
-output_dir="$HOME/ADIS_project24/datasources/population_scripts/mongodb"
-tables_catalog="$HOME/ADIS_project24/data/csv_data/tables_catalog.txt"
-data_dir="$HOME/ADIS_project24/data/csv_data"
+output_dir="$HOME/ADIS_project24/datasources/table_population_scripts/mongodb"
+tables_catalog="$HOME/ADIS_project24/data/tables_catalog.txt"
+data_dir="/data"
 
-if [ ! -d "$output_dir" ]; then
-    mkdir -p "$output_dir"
-fi
+# create new directory where table scripts are going to reside 
+rm -rf "$output_dir" && mkdir -p "$output_dir"
 
-
+# make the files
 create_files() {
     local table="$1"
 
-    if grep -iq "$table" "$tables_catalog"; then
+    if grep -iq "^$table$" "$tables_catalog"; then     
+        # create output directory file
+        mongo_file="$output_dir/${table}.sh"
 
-        
-        # match table with input file
+        # generate string of fields separated by comma
+        fields=$(~/ADIS_project24/utils/generate_fields.sh "$table")
 
-        # create a list of the fields of the table
+        # copy population command to file 
+        echo "mongoimport --uri="mongodb://root:root@localhost:27017/?authSource=admin" \
+            --db='adis' \
+            --collection='$table' \
+            --type=csv \
+            --file=/mongo_data/$table.csv \
+            --fields=' "$fields"' " >> "$mongo_file"
+        printf "\n" >> "$mongo_file"
 
-        # create file with import command
-
-        mongoimport --uri="mongodb://root:root@localhost:27017/?authSource=admin" \
-                    --db='adis' \
-                    --collection='income_band' \
-                    --type=csv \
-                    --file=/mongo_data/income_band.csv \
-                    --fields="ib_income_band_sk, ib_lower_bound, ib_upper_bound" 
-
+        echo "Created $table.sh"
     else
         echo "Table Name: $table doesn't match with any dataset from tables_catalog.txt"
-
     fi
 }
 
+# get all table names and iterate over to create init_script for each table
 grep -oP '(?<=create table\s)[a-zA-Z0-9_]+' "$input_file" | \
-while read table; do
-    create_files "$table"
-done 
+while read table_name; do
+    create_files "$table_name"
+done
