7a107b25156e7518307d649c24eb0c2aa10e488f
gmitis <gi.muths@gmail.com>
2025-02-02 18:34:32 +0000

    The difference is a space in the fields parameter of mongoimport. Mongo works Im not taking a chance :')

--- datasources/table_population_scripts/mongodb/catalog_page.sh
+++ datasources/table_population_scripts/mongodb/catalog_page.sh
@@ -1,2 +1,2 @@
-mongoimport --uri=mongodb://root:root@localhost:27017/?authSource=admin             --db='adis'             --collection='catalog_page'             --type=csv             --file=/mongo_data/catalog_page.csv             --fields='cp_catalog_page_sk,cp_catalog_page_id,cp_start_date_sk,cp_end_date_sk,cp_department,cp_catalog_number,cp_catalog_page_number,cp_description,cp_type' 
+mongoimport --uri=mongodb://root:root@localhost:27017/?authSource=admin             --db='adis'             --collection='catalog_page'             --type=csv             --file=/mongo_data/catalog_page.csv             --fields=' cp_catalog_page_sk,cp_catalog_page_id,cp_start_date_sk,cp_end_date_sk,cp_department,cp_catalog_number,cp_catalog_page_number,cp_description,cp_type' 
 
--- datasources/table_population_scripts/mongodb/inventory.sh
+++ datasources/table_population_scripts/mongodb/inventory.sh
@@ -1,2 +1,2 @@
-mongoimport --uri=mongodb://root:root@localhost:27017/?authSource=admin             --db='adis'             --collection='inventory'             --type=csv             --file=/mongo_data/inventory.csv             --fields='inv_date_sk,inv_item_sk,inv_warehouse_sk,inv_quantity_on_hand' 
+mongoimport --uri=mongodb://root:root@localhost:27017/?authSource=admin             --db='adis'             --collection='inventory'             --type=csv             --file=/mongo_data/inventory.csv             --fields=' inv_date_sk,inv_item_sk,inv_warehouse_sk,inv_quantity_on_hand' 
 
