349cc32d209c2e650558427d5cad4e023f93bf24
gmitis <gi.muths@gmail.com>
2025-02-06 10:02:33 +0000

    Corrected generate_queries.sh to include scale as a parameter

--- utils/generate_queries.sh
+++ utils/generate_queries.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-TOOLS_DIR="/home/ubuntu/ADIS_project24/DSGen/tools"
-TEMPLATES_DIR="/home/ubuntu/ADIS_project24/DSGen/query_templates"  # Replace with the actual path to query templates
-OUTPUT_DIR="/home/ubuntu/ADIS_project24/queries"             # Replace with the desired output directory
+TOOLS_DIR="$HOME/ADIS_project24/DSGen/tools"
+TEMPLATES_DIR="$HOME/ADIS_project24/DSGen/query_templates"  # Replace with the actual path to query templates
+OUTPUT_DIR="$HOME/ADIS_project24/utils/initial_queries/test"             # Replace with the desired output directory
 DIALECT="ansi"                           # Dialect for the queries
-SCALE=10   
+SCALE="$1"
 
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 [data_scale_factor:integer]"
+fi
 
 cd "$TOOLS_DIR" || { echo "Failed to change directory to $TOOLS_DIR"; exit 1; }
 
+# remove any queries left from before
+rm -rf "$OUTPUT_DIR"/*
 
 for i in $(seq 1 99); do
     TEMPLATE="query${i}.tpl"  # Query template file
     OUTPUT_FILE="$OUTPUT_DIR/query${i}.sql"  # Desired output file
-
+c
     echo "Generating $OUTPUT_FILE from $TEMPLATE..."
 
     # Run the dsqgen command for the current query template
