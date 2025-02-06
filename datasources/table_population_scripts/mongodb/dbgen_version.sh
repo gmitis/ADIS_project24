ccddf66dae8fc228c000cad9150b238e09974f8a
gmitis <gi.muths@gmail.com>
2025-02-06 10:03:10 +0000

    Corrected MongoDB table definitions

--- datasources/table_population_scripts/mongodb/dbgen_version.sh
+++ datasources/table_population_scripts/mongodb/dbgen_version.sh
@@ -1,2 +1,2 @@
-mongoimport --uri=mongodb://root:root@localhost:27017/?authSource=admin             --db='adis'             --collection='dbgen_version'             --type=csv             --file=/mongo_data/dbgen_version.csv             --fields=' dv_version,dv_create_date,dv_create_time,dv_cmdline_args' 
+mongoimport --uri=mongodb://root:root@localhost:27017/?authSource=admin             --db='adis'             --collection='dbgen_version'             --type=csv             --file=/mongo_data/dbgen_version.csv             --fields='dv_version,dv_create_date,dv_create_time,dv_cmdline_args' 
 
--- datasources/table_population_scripts/mongodb/time_dim.sh
+++ datasources/table_population_scripts/mongodb/time_dim.sh
@@ -1,2 +1,2 @@
-mongoimport --uri=mongodb://root:root@localhost:27017/?authSource=admin             --db='adis'             --collection='time_dim'             --type=csv             --file=/mongo_data/time_dim.csv             --fields=' t_time_sk,t_time_id,t_time,t_hour,t_minute,t_second,t_am_pm,t_shift,t_sub_shift,t_meal_time' 
+mongoimport --uri=mongodb://root:root@localhost:27017/?authSource=admin             --db='adis'             --collection='time_dim'             --type=csv             --file=/mongo_data/time_dim.csv             --fields='t_time_sk,t_time_id,t_time,t_hour,t_minute,t_second,t_am_pm,t_shift,t_sub_shift,t_meal_time' 
 
